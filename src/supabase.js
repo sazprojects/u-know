195ecf5e47b387112d3cdda084a298967e7c1f3e
Saz <120389249+sazprojects@users.noreply.github.com>
2023-12-03 22:25:40 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://bycxmewfyvxcdrycweft.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
